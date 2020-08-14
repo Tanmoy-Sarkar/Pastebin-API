9c24cb75597aaa3df13d7bb4cd84528d31b95d0b
Tanmoy Sarkar <tanmoy9920@gmail.com>
2020-08-14 18:19:04 +0600

    adding some extra fields in model

--- snippets/admin.py
+++ snippets/admin.py
@@ -2,5 +2,7 @@ from django.contrib import admin
 from .models import Snippet
 
 # Register your models here.
+class SnippetAdmin(admin.ModelAdmin):
+    readonly_fields = ('highlighted',)
 
-admin.site.register(Snippet)
+admin.site.register(Snippet,SnippetAdmin)
