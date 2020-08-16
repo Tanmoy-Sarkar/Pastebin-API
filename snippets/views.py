06d3f5021e8e4500dab1d0cfab41def2e83be862
Tanmoy Sarkar <tanmoy9920@gmail.com>
2020-08-16 11:49:08 +0600

    hyperlinking api

--- snippets/views.py
+++ snippets/views.py
@@ -1,6 +1,6 @@
 
 from django.contrib.auth.models import User
-from rest_framework import generics,permissions
+from rest_framework import generics,permissions,renderers
 from .models import Snippet
 from .permissions import IsOwnerOrReadOnly
 from .serializers import SnippetSerializer,UserSerializer
@@ -38,3 +38,11 @@ class UserList(generics.ListAPIView):
 class UserDetail(generics.RetrieveAPIView): 
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+class SnippetHighlight(generics.GenericAPIView): # new
+    queryset = Snippet.objects.all()
+    renderer_classes = (renderers.StaticHTMLRenderer,)
+
+    def get(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
